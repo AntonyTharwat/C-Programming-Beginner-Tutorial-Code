4bd55017f6f2d0353f10d833e880dbd48a6f4e8f
AntonyTharwat <84070221+AntonyTharwat@users.noreply.github.com>
2023-03-06 11:47:43 +0200

    FirstCommit

---
+++ TutorialsCode/src/DataTypes.c
@@ -0,0 +1,30 @@
+/*
+ * DataTypes.c
+ *
+ *  Created on: Mar 5, 2023
+ *      Author: MBR
+ */
+
+# include <stdio.h>
+
+void main() {
+
+	double no = 17.5; //double data
+
+	float fl = 78.1f; //float data type
+
+	double noo = 5.5e6; //double data type
+
+	char character = 'z'; //char data type
+
+	printf("%.2lf \n", no); //print double value
+	printf("%.2f \n", fl); // print float value
+	printf("%.0lf \n ", noo); //print exponential value
+
+	printf("%c \n",character); //print character
+	printf("%d \n", character); //print character as an integer value
+
+	printf("size of double = %d \r\n", sizeof(no));
+	printf("size of character = %d", sizeof(character));
+	return 0;
+}
