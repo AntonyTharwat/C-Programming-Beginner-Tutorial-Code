0930ddbf7f22a53ac7bdc2dabf078d6770cd5708
AntonyTharwat <84070221+AntonyTharwat@users.noreply.github.com>
2023-03-06 14:40:06 +0200

    firstcommit

---
+++ TutorialCodePractice/src/GetUserInput.c
@@ -0,0 +1,23 @@
+/*
+ * GetUserInput.c
+ *
+ *  Created on: Mar 6, 2023
+ *      Author: MBR
+ */
+
+/*
+#include <stdio.h>
+
+void main(){
+
+	int age;
+
+	printf("Please enter the age number: \n");
+
+	scanf("%d", &age);
+
+	printf("Age is = %d", age);
+
+	return 0;
+}
+*/
