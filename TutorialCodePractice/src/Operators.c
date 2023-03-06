b5461e4d8097d8eeef2f5dc38154ae18cdf31c54
AntonyTharwat <84070221+AntonyTharwat@users.noreply.github.com>
2023-03-06 14:55:42 +0200

    commited

--- TutorialCodePractice/src/Operators.c
+++ TutorialCodePractice/src/Operators.c
@@ -6,12 +6,32 @@
  Copyright   : Your copyright notice
  Description : Hello World in C, Ansi-style
  ============================================================================
+ + 		Addition
+ - 		Subtraction
+ * 		Multiplication
+ / 		Division
+ % 		Reminder
+ ++ 	Increment
+ -- 	decrement
+
 */
 
 #include <stdio.h>
 #include <stdlib.h>
 
+int add(int x, int y)
+{
+	return x+ y;
+	}
 int main(void) {
-	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
-	return EXIT_SUCCESS;
+
+	int x = 4, y = 20;
+
+	printf("please enter two numbers: \n");
+
+//	scanf("\n %d %d", x, y);
+
+	printf("Sum is equal = %d", add(x,y));
+
+	return 0;
 }
