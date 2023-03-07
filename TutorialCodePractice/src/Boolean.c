e1bae1d2d87e0265a03d8ffa99285c88adeda29b
AntonyTharwat <84070221+AntonyTharwat@users.noreply.github.com>
2023-03-07 22:18:55 +0200

    commited

---
+++ TutorialCodePractice/src/Boolean.c
@@ -0,0 +1,23 @@
+///*
+// * TypeConversion.c
+// *
+// *  Created on: Mar 7, 2023
+// *      Author: MBR
+// */
+//
+//#include <stdio.h>
+//#include <stdint.h>
+//#include <stdbool.h>
+//
+//int main(){
+//
+//bool value1 = true;
+//
+//bool value2 = false;
+//
+//printf("%d\n", value1);
+//
+//printf("%d",value2);
+//
+//return 0;
+//}
