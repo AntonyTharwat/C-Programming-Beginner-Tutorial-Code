6bda1242e446640d0e4174c30f10aff292a7fb25
AntonyTharwat <84070221+AntonyTharwat@users.noreply.github.com>
2023-03-08 00:48:22 +0200

    commited

--- TutorialCodePractice/src/IfElse.c
+++ TutorialCodePractice/src/IfElse.c
@@ -1,19 +1,19 @@
-#include <stdio.h>
-#include <stdint.h>
-#include <stdbool.h>
-
-int main(){
-
-	int x;
-
-	scanf("%d", &x);
-
-	if(x > 0){
-		printf("The number is positive \n");
-	}else if (x < 0){
-		printf("THe number is negative \n");
-	}else{
-		printf("Equal Zero 0 \n");
-	}
-	return 0;
-}
+//#include <stdio.h>
+//#include <stdint.h>
+//#include <stdbool.h>
+//
+//int main(){
+//
+//	int x;
+//
+//	scanf("%d", &x);
+//
+//	if(x > 0){
+//		printf("The number is positive \n");
+//	}else if (x < 0){
+//		printf("THe number is negative \n");
+//	}else{
+//		printf("Equal Zero 0 \n");
+//	}
+//	return 0;
+//}
