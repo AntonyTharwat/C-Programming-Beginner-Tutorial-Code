83fadcd7342e58b544be192162a880437b2061e8
AntonyTharwat <84070221+AntonyTharwat@users.noreply.github.com>
2023-03-08 15:12:14 +0200

    newcommit

---
+++ TutorialCodePractice/src/PointerFunction.c
@@ -0,0 +1,16 @@
+///*
+// * PointerFunction.c
+// *
+// *  Created on: Mar 8, 2023
+// *      Author: MBR
+// */
+//
+//#include<stdio.h>
+//#include<stdint.h>
+//
+//int main(){
+//
+//	printf("Hello World \n");
+//
+//	return 0;
+//}
